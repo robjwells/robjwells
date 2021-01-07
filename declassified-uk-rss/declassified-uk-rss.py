ed6109e76f9eeba0f23ab003ec0be8019fe6fe30
Rob Wells <rob@robjwells.com>
2021-01-07 08:12:27 +0000

    Declassified filter: Remove blank lines

--- declassified-uk-rss/declassified-uk-rss.py
+++ declassified-uk-rss/declassified-uk-rss.py
@@ -10,13 +10,16 @@ for item in s.find_all("item"):
     if "declassified" not in item.find("category").text.lower():
         item.decompose()
 
-url = 'https://robjwells.github.io/robjwells/declassified-uk-rss/filtered.xml'
-s.find('channel').find('link')['href'] = url
+url = "https://robjwells.github.io/robjwells/declassified-uk-rss/filtered.xml"
+s.find("channel").find("link")["href"] = url
 
 comments = s.find_all(text=lambda text: isinstance(text, Comment))
 for comment in comments:
     _ = comment.extract()
 
 if s.find_all("item"):
+    as_string = str(s)
+    fewer_breaks = "\n".join([line for line in as_string.split("\n") if line])
+    print(fewer_breaks)
     with open("filtered.xml", "wb") as out:
-        out.write(s.encode("utf-8"))
+        out.write(fewer_breaks.encode("utf-8"))
