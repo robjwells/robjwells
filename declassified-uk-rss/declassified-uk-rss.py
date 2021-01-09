472fbce7b512a35188a058f0c9542a865ccaadbf
Rob Wells <rob@robjwells.com>
2021-01-09 11:21:43 +0000

    Declassified filter: Refactor, only write substantial changes

--- declassified-uk-rss/declassified-uk-rss.py
+++ declassified-uk-rss/declassified-uk-rss.py
@@ -1,23 +1,76 @@
-#!/usr/local/bin/python3
+from pathlib import Path
+from typing import Set
 
 import requests
 from bs4 import BeautifulSoup, Comment
 
-r = requests.get("https://www.dailymaverick.co.za/dmrss/")
-s = BeautifulSoup(r.content, "lxml-xml")
+OUTPUT_FILENAME: str = "filtered.xml"
 
-for item in s.find_all("item"):
-    if "declassified" not in item.find("category").text.lower():
-        item.decompose()
 
-url = "https://robjwells.github.io/robjwells/declassified-uk-rss/filtered.xml"
-s.find("channel").find("link")["href"] = url
+def fetch_rss_feed() -> BeautifulSoup:
+    r = requests.get("https://www.dailymaverick.co.za/dmrss/")
+    return BeautifulSoup(r.content, "lxml-xml")
 
-comments = s.find_all(text=lambda text: isinstance(text, Comment))
-for comment in comments:
-    _ = comment.extract()
 
-if s.find_all("item"):
-    fewer_breaks = "\n".join([line for line in str(s).split("\n") if line])
-    with open("filtered.xml", "wb") as out:
-        out.write(fewer_breaks.encode("utf-8"))
+def remove_non_declassified_items(soup: BeautifulSoup) -> None:
+    """Mutates `soup`."""
+    for item in soup.find_all("item"):
+        if "declassified" not in item.find("category").text.lower():
+            item.decompose()
+
+
+def replace_rss_link_with_mine(soup: BeautifulSoup, *, filename: str = OUTPUT_FILENAME) -> None:
+    """Mutates `soup`."""
+    url = f"https://robjwells.github.io/robjwells/declassified-uk-rss/{filename}"
+    soup.find("channel").find("link")["href"] = url
+
+
+def remove_comments(soup: BeautifulSoup) -> None:
+    """Mutates `soup`."""
+    comments = soup.find_all(text=lambda text: isinstance(text, Comment))
+    for comment in comments:
+        _ = comment.extract()
+
+
+def prepare_soup() -> BeautifulSoup:
+    soup = fetch_rss_feed()
+    remove_non_declassified_items(soup)
+    replace_rss_link_with_mine(soup)
+    remove_comments(soup)
+    return soup
+
+
+def contains_items(soup: BeautifulSoup) -> bool:
+    return bool(soup.find_all("item"))
+
+
+def remove_extra_line_breaks(xml: str) -> str:
+    return "\n".join([line for line in xml.splitlines() if line])
+
+
+def filter_out_uninteresting_xml_lines(xml: str) -> Set[str]:
+    return {
+        line
+        for line in xml.splitlines()
+        if line
+        if (not line.startswith("<pubDate>") and not line.startswith("<lastBuildDate>"))
+    }
+
+
+def more_than_date_has_changed(
+    xml: str, *, output_filename: str = OUTPUT_FILENAME
+) -> bool:
+    out = Path(output_filename)
+    if not out.exists():
+        return True
+    previous_set = filter_out_uninteresting_xml_lines(out.read_text())
+    current_set = filter_out_uninteresting_xml_lines(xml)
+    return bool(current_set - previous_set)
+
+
+def main(*, output_filename: str = OUTPUT_FILENAME) -> None:
+    soup = prepare_soup()
+    xml = remove_extra_line_breaks(str(soup))
+
+    if contains_items(soup) and more_than_date_has_changed(xml):
+        Path(output_filename).write_text(xml)
